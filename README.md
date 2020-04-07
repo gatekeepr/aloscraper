8c1b4125fe1068699d2615d88bb5002fe436b4d9
hholm <henrikholm@web.de>
2020-04-07 16:35:44 +0200

    code cleanup and automated login

--- README.md
+++ README.md
@@ -5,18 +5,16 @@ Download courses from alomoves.com automatically.
 
 1. ```pip install selenium```
 2. Download Chrome Webdriver according to your Version (probably 80) [HERE](https://chromedriver.chromium.org/downloads) and place into your working folder next to the .py (or add it to your PATH)
-3. Adjust lines 8 to 12 depending on your OS and desired download paths.
-4. Make sure you have a valid alomoves account on hand (2 week test accounts are fine).
-5. Place the links of courses you want to download in **downloadlinks.txt**, one per line.
+3. Make sure you have a valid alomoves account on hand (2 week test accounts are fine).
+4. Place the links of courses you want to download in **downloadlinks.txt**, one per line.
     - Example: https://www.alomoves.com/series/COURSENAME/workouts
     - You find these links by navigating to the course page and find the one where all the individual videos are shown with thumbnails.
 
 ## Usage
 1. ```python3 downloader.py```
-2. A chrome browser will open with the alomoves login page.
-3. You have 25 seconds to login to your account.
-4. After login just wait and the downloads will begin automatically.
-5. You can minimize the browser and watch the console for progress.
+2. Enter your credentials in the terminal when asked.
+3. After login just wait and the downloads will begin automatically.
+4. You can minimize the browser and watch the console for progress.
 
               __
           ___( o)>
--- downloader.py
+++ downloader.py
@@ -3,66 +3,102 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
 import time
+import getpass
 
-# set absolute paths and variables according to your OS
-# LINKFILE = "/your/unix/path/downloadlinks.txt"
-LINKFILE = r"C:\Users\USERNAME\Documents\aloscraper\downloadlinks.txt"
-# BASEPATH = "/your/unix/path/content"
-BASEPATH = r"C:\Users\USERNAME\Documents\aloscraper"
-WINDOWS = True
-
-# read config and prepare data
+browser = webdriver.Chrome()
 paths = []
 lines = []
-with open(LINKFILE) as f:
-    lines = [line.rstrip() for line in f]
-for line in lines:
-    paths.append(BASEPATH +
-                 line.split("series")[1].split("workouts")[0])
-if WINDOWS:
+LINKFILE = ""
+BASEPATH = ""
+SYSTEM = os.name
+
+if SYSTEM == "nt":
+    LINKFILE = os.getcwd() + "\\downloadlinks.txt"
+    with open(LINKFILE) as f:
+        lines = [line.rstrip() for line in f]
+    BASEPATH = os.getcwd() + "\\content"
+    for line in lines:
+        line = line.split("series")[1].split("workouts")[0]
+        paths.append(BASEPATH + "\\" + line[1:-1]
+                     + "\\")
     for path in paths:
         path.replace("/", "\\")
-
-
-# download mp4 with urllib
+elif SYSTEM == "posix":
+    LINKFILE = os.getcwd() + "/downloadlinks.txt"
+    with open(LINKFILE) as f:
+        lines = [line.rstrip() for line in f]
+    BASEPATH = os.getcwd() + "/content"
+    for line in lines:
+        line = line.split("series")[1].split("workouts")[0]
+        paths.append(BASEPATH + line)
+else:
+    print("OS not supported, please open an issue on Github.")
 
 
 def downloadmp4(url, filename, path):
-    print("Downloading: " + filename)
-    urllib.request.urlretrieve(
-        url, path + filename + ".mp4")
+    fullname = path + filename + ".mp4"
+    if os.path.isfile(fullname):
+        print(filename + " already exists, skipping...")
+        return()
+    else:
+        print("Downloading: " + filename)
+        urllib.request.urlretrieve(
+            url, fullname)
 
 
-# initialize webdriver and do a manual login
-browser = webdriver.Chrome()
-browser.get(
-    "https://www.alomoves.com/signin")
-time.sleep(25)
+def doLogin(email, password):
+    browser.get(
+        "https://www.alomoves.com/signin")
+    time.sleep(5)
+    mailfield = browser.find_elements_by_xpath(
+        "//input[contains(@name,'email')]")[1]
+    pwfield = browser.find_elements_by_xpath(
+        "//input[contains(@name,'password')]")[2]
+    mailfield.send_keys(email)
+    mailfield.send_keys(Keys.TAB)
+    time.sleep(1)
+    pwfield.send_keys(password)
+    pwfield.send_keys(Keys.ENTER)
 
-# run the download routine for all courses
-for i in range(len(lines)):
 
-    # grab all video links by searching the divs
-    browser.get(lines[i])
+def collectClasses(courselink):
+    print(f"== Grabbing links for {courselink} ==")
+    browser.get(courselink)
     workoutLinks = browser.find_elements_by_xpath(
         "//div[contains(@class,'workout-title')]/a")
     reallinks = [link.get_attribute("href") for link in workoutLinks]
+    return reallinks
 
-    # create directory for the course
+
+def grabLesson(lessonlink, path):
+    browser.get(lessonlink)
+    time.sleep(5)
+    videolink = browser.find_element_by_tag_name(
+        "video").get_attribute("currentSrc")
+    videotitle = browser.find_element_by_tag_name(
+        "h1").get_attribute("innerText").replace(":", "").replace(" ", "_").replace("/", "")
+    downloadmp4(videolink, videotitle, path)
+
+
+def makeDir(dirr):
     try:
-        os.makedirs(paths[i])
+        os.makedirs(dirr)
     except OSError:
         pass
 
-    # call download routine for every available link
-    print("== Starting Course: " + paths[i] + " ==")
-    for link in reallinks:
-        browser.get(link)
-        time.sleep(10)
-        videolink = browser.find_element_by_tag_name(
-            "video").get_attribute("currentSrc")
-        videotitle = browser.find_element_by_tag_name(
-            "h1").get_attribute("innerText").replace(":", "").replace(" ", "_").replace("/", "")
-        downloadmp4(videolink, videotitle, paths[i])
-browser.quit()
-print("++ All downloads completed successfully, have fun! ++")
+
+def main():
+    email = input('Enter Email:')
+    password = getpass.getpass(prompt='Enter Password:')
+    doLogin(email, password)
+    quit()
+    for i in range(len(lines)):
+        lessonlinks = collectClasses(lines[i])
+        makeDir(paths[i])
+        for link in lessonlinks:
+            grabLesson(link, paths[i])
+    browser.quit()
+    print("++ All downloads completed successfully, have fun! ++")
+
+
+main()
